1d3bd5d65961936dbabbe8a5b50ba542f37b913c
Phuong Dao Hoai <daophuong131.work@gmail.com>
2024-10-28 02:44:35 +0700

    update: bottom tab
    - fix lại thứ tự của bottom tab
    - Service - Stylist - Booking - Voucher - Profile

--- app/index.jsx
+++ app/index.jsx
@@ -4,7 +4,7 @@ import { createStackNavigator } from "@react-navigation/stack";
 import AuthIntroScreen from "../src/auth/AuthIntroScreen";
 import LoginScreen from "../src/auth/LoginScreen";
 import SignupScreen from "../src/auth/SignupScreen";
-import HomeScreen from "../src/screens/HomeScreen";
+import StylistScreen from "../src/screens/StylistScreen";
 import BottomTabNavigator from "../src/navigation/BottomTabNavigator";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import { persistor, store } from "../src/store/store";
@@ -16,37 +16,37 @@ const Stack = createStackNavigator();
 export default function App() {
 	return (
 		<Provider store={store}>
-		<PersistGate loading={null} persistor={persistor}>
-			<NavigationContainer independent={true}>
-				<Stack.Navigator initialRouteName="AuthIntroScreen">
-					<Stack.Screen
-						name="AuthIntroScreen"
-						component={AuthIntroScreen}
-						options={{ headerShown: false }}
-					/>
-					<Stack.Screen
-						name="Login"
-						component={LoginScreen}
-						options={{ headerShown: false }}
-					/>
-					<Stack.Screen
-						name="Signup"
-						component={SignupScreen}
-						options={{ headerShown: false }}
-					/>
-					<Stack.Screen
-						name="HomeScreen"
-						component={BottomTabNavigator} // Điều hướng tới BottomTabNavigator
-						options={{ headerShown: false }}
-					/>
-					<Stack.Screen
-						name="ServiceChoosing"
-						component={ServiceChoosing} // Điều hướng tới BottomTabNavigator
-						options={{ headerShown: false }}
-					/>
-				</Stack.Navigator>
-			</NavigationContainer>
-		</PersistGate>
+			<PersistGate loading={null} persistor={persistor}>
+				<NavigationContainer independent={true}>
+					<Stack.Navigator initialRouteName="AuthIntroScreen">
+						<Stack.Screen
+							name="AuthIntroScreen"
+							component={AuthIntroScreen}
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name="Login"
+							component={LoginScreen}
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name="Signup"
+							component={SignupScreen}
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name="ServiceScreen"
+							component={BottomTabNavigator} // Điều hướng tới BottomTabNavigator
+							options={{ headerShown: false }}
+						/>
+						<Stack.Screen
+							name="ServiceChoosing"
+							component={ServiceChoosing} // Điều hướng tới BottomTabNavigator
+							options={{ headerShown: false }}
+						/>
+					</Stack.Navigator>
+				</NavigationContainer>
+			</PersistGate>
 		</Provider>
 	);
 }
