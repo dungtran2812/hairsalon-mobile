cc5f3386b23555dbf590dffe6845f330d4d6b415
Phuong Dao Hoai <daophuong131.work@gmail.com>
2024-10-30 23:15:55 +0700

    update: Bottomtab ui
    - chỉnh màu

--- src/navigation/BottomTabNavigator.jsx
+++ src/navigation/BottomTabNavigator.jsx
@@ -1,68 +1,91 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ServiceScreen from "../screens/ServiceScreen/ServiceScreen";
-import StylistScreen from "../screens/StylistScreen"; // Thêm màn hình Stylist
+import StylistScreen from "../screens/StylistScreen";
 import BookingScreen from "../screens/BookingScreen";
 import VoucherScreen from "../screens/VoucherScreen";
 import Icon from "react-native-vector-icons/Ionicons";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import ProfileStack from "./stack/ProfileStack";
-import BookingStack from "./stack/BookingStack";
 
 const Tab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
-  return (
-    <SafeAreaProvider>
-      <Tab.Navigator
-        screenOptions={({ route }) => ({
-          tabBarIcon: ({ focused, color, size }) => {
-            let iconName;
+	return (
+		<SafeAreaProvider>
+			<Tab.Navigator
+				screenOptions={({ route }) => ({
+					tabBarIcon: ({ focused, color, size }) => {
+						let iconName;
 
-            if (route.name === "Service") {
-              iconName = focused ? "cut" : "cut-outline";
-            } else if (route.name === "Stylist") {
-              iconName = focused ? "people" : "people-outline";
-            } else if (route.name === "Booking") {
-              iconName = focused ? "calendar" : "calendar-outline";
-            } else if (route.name === "Voucher") {
-              iconName = focused ? "pricetag" : "pricetag-outline";
-            } else if (route.name === "Profile") {
-              iconName = focused ? "person-circle" : "person-circle-outline";
-            }
+						if (route.name === "Service") {
+							iconName = focused ? "cut" : "cut-outline";
+						} else if (route.name === "Stylist") {
+							iconName = focused ? "people" : "people-outline";
+						} else if (route.name === "Booking") {
+							iconName = focused
+								? "calendar"
+								: "calendar-outline";
+						} else if (route.name === "Voucher") {
+							iconName = focused
+								? "pricetag"
+								: "pricetag-outline";
+						} else if (route.name === "Profile") {
+							iconName = focused
+								? "person-circle"
+								: "person-circle-outline";
+						}
 
-            return <Icon name={iconName} size={size} color={color} />;
-          },
-          tabBarActiveTintColor: "rgb(97, 70, 59)",
-          tabBarInactiveTintColor: "gray",
-        })}
-      >
-        <Tab.Screen
-          name="Service"
-          component={ServiceScreen}
-          options={{ headerShown: false }}
-        />
-        <Tab.Screen
-          name="Stylist"
-          component={StylistScreen}
-          options={{ headerShown: false }}
-        />
-        <Tab.Screen
-          name="Booking"
-          component={BookingScreen}
-          options={{ headerShown: false }}
-        />
-        <Tab.Screen
-          name="Voucher"
-          component={VoucherScreen}
-          options={{ headerShown: false }}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileStack}
-          options={{ headerShown: false }}
-        />
-      </Tab.Navigator>
-    </SafeAreaProvider>
-  );
+						let iconSize = focused ? 28 : 24; // Tăng kích thước cho biểu tượng khi được chọn
+						return (
+							<Icon
+								name={iconName}
+								size={iconSize}
+								color={color}
+							/>
+						);
+					},
+					tabBarActiveTintColor: "#5D3A29",
+					tabBarInactiveTintColor: "gray",
+					tabBarStyle: {
+						backgroundColor: "#FAF3E0",
+						borderTopWidth: 0,
+						elevation: 5,
+						height: 60,
+						borderRadius: 15,
+						position: "absolute",
+						left: 0,
+						right: 0,
+						bottom: 0,
+					},
+				})}
+			>
+				<Tab.Screen
+					name="Service"
+					component={ServiceScreen}
+					options={{ headerShown: false }}
+				/>
+				<Tab.Screen
+					name="Stylist"
+					component={StylistScreen}
+					options={{ headerShown: false }}
+				/>
+				<Tab.Screen
+					name="Booking"
+					component={BookingScreen}
+					options={{ headerShown: false }}
+				/>
+				<Tab.Screen
+					name="Voucher"
+					component={VoucherScreen}
+					options={{ headerShown: false }}
+				/>
+				<Tab.Screen
+					name="Profile"
+					component={ProfileStack}
+					options={{ headerShown: false }}
+				/>
+			</Tab.Navigator>
+		</SafeAreaProvider>
+	);
 }
