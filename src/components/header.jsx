4e5ea0d5b27f6cc98bc9bb148b88774682a79cf0
Phuong Dao Hoai <daophuong131.work@gmail.com>
2024-10-30 23:16:50 +0700

    update: ServiceScreen ui v1
    - chưa có thêm những trang khác

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,94 +1,103 @@
 import React from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
-import imgProfile from "../../scripts/assets/FunFace.jpg";
 import Icon from "react-native-vector-icons/AntDesign";
 import { useGetUserInforQuery } from "../services/hairsalon.service";
 
 const header = () => {
-  const { data: userInfo} = useGetUserInforQuery();
-  const role = userInfo?.user?.role && Array.isArray(userInfo?.user?.role) ? userInfo?.user?.role[0] : "Unknown role";
-  return (
-    <View style={stylesProfile.container}>
-      <View style={stylesProfile.infor}>
-        <View style={stylesProfile.image}>
-          <Image source={{ uri: userInfo?.user?.avatar }} style={stylesProfile.avatar} />
-        </View>
-        <View style={stylesProfile.username}>
-          <Text style={stylesProfile.name}>{userInfo?.user?.name}</Text>
-          <Text style={stylesProfile.linklog}>
-            {role}{" "}<Icon name="right" size={12} color="white" />
-          </Text>
-        </View>
-      </View>
-      <View style={stylesProfile.otherIcon}>
-        <View style={stylesProfile.otherIconContainer}>
-          <Icon name="bells" size={25} color="white" />
-        </View>
-        <View style={stylesProfile.loyalPoint}>
-          <Text style={stylesProfile.loyalText}>Điểm: {userInfo?.user?.loyaltyPoints}</Text>
-        </View>
-      </View>
-    </View>
-  );
+	const { data: userInfo } = useGetUserInforQuery();
+	const role =
+		userInfo?.user?.role && Array.isArray(userInfo?.user?.role)
+			? userInfo?.user?.role[0]
+			: "Unknown role";
+	return (
+		<View style={stylesProfile.container}>
+			<View style={stylesProfile.infor}>
+				<View style={stylesProfile.image}>
+					<Image
+						source={{ uri: userInfo?.user?.avatar }}
+						style={stylesProfile.avatar}
+					/>
+				</View>
+				<View style={stylesProfile.username}>
+					<Text style={stylesProfile.name}>
+						{userInfo?.user?.name}
+					</Text>
+					<Text style={stylesProfile.linklog}>
+						{role} <Icon name="right" size={12} color="white" />
+					</Text>
+				</View>
+			</View>
+			<View style={stylesProfile.otherIcon}>
+				<View style={stylesProfile.otherIconContainer}>
+					<Icon name="bells" size={25} color="white" />
+				</View>
+				<View style={stylesProfile.loyalPoint}>
+					<Text style={stylesProfile.loyalText}>
+						Điểm: {userInfo?.user?.loyaltyPoints}
+					</Text>
+				</View>
+			</View>
+		</View>
+	);
 };
 const stylesProfile = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginTop: 15,
-    marginHorizontal: 10,
-    height: 100,
-    flexDirection: "row",
-  },
-  infor: {
-    flex: 2,
-    flexDirection: "row",
-  },
-  image: {
-    width: 60,
-    height: 60,
-    marginRight: 10,
-  },
-  avatar: {
-    width: "100%",
-    height: "100%",
-    borderRadius: 100,
-    borderWidth: 1,
-    borderColor: "white",
-  },
-  username: {
-    marginTop: 10,
-  },
-  name: {
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "white",
-  },
-  linklog: {
-    fontSize: 12,
-    color: "white",
-  },
-  otherIcon: {
-    flex: 1,
-  },
-  otherIconContainer: {
-    flexDirection: "row",
-    justifyContent: "flex-end",
-    gap: 20,
-    marginRight: 10,
-  },
-  loyalPoint: {
-    borderColor: "white",
-    borderWidth: 1,
-    borderColor: "white",
-    borderRadius: 50,
-    marginTop: 10,
-    height: 30,
-    justifyContent: "center",
-  },
-  loyalText: {
-    textAlign: "center",
-    color: "white",
-  },
+	container: {
+		flex: 1,
+		marginTop: 15,
+		marginHorizontal: 10,
+		height: 100,
+		flexDirection: "row",
+	},
+	infor: {
+		flex: 2,
+		flexDirection: "row",
+	},
+	image: {
+		width: 60,
+		height: 60,
+		marginRight: 10,
+	},
+	avatar: {
+		width: "100%",
+		height: "100%",
+		borderRadius: 100,
+		borderWidth: 1,
+		borderColor: "white",
+	},
+	username: {
+		marginTop: 10,
+	},
+	name: {
+		fontSize: 20,
+		fontWeight: "bold",
+		color: "white",
+	},
+	linklog: {
+		fontSize: 12,
+		color: "white",
+	},
+	otherIcon: {
+		flex: 1,
+	},
+	otherIconContainer: {
+		flexDirection: "row",
+		justifyContent: "flex-end",
+		gap: 20,
+		marginRight: 10,
+	},
+	loyalPoint: {
+		borderColor: "white",
+		borderWidth: 2,
+		borderColor: "white",
+		borderRadius: 50,
+		marginTop: 10,
+		height: 30,
+		justifyContent: "center",
+	},
+	loyalText: {
+		textAlign: "center",
+		color: "white",
+	},
 });
 
 export default header;
--- src/screens/ServiceScreen/CardService.jsx
+++ src/screens/ServiceScreen/CardService.jsx
@@ -5,136 +5,154 @@ import { TouchableOpacity } from "react-native";
 import { useNavigation } from "expo-router";
 
 const CardService = () => {
-  const navigation = useNavigation();
-  const { data: serviceInfo, error, isLoading } = useViewServiceQuery();
+	const navigation = useNavigation();
+	const { data: serviceInfo, error, isLoading } = useViewServiceQuery();
 
-  // Handle loading and error states
-  if (isLoading) {
-    return <Text>Loading...</Text>;
-  }
+	// Handle loading and error states
+	if (isLoading) {
+		return <Text>Loading...</Text>;
+	}
 
-  if (error) {
-    return <Text>Error: {error.message}</Text>;
-  }
+	if (error) {
+		return <Text>Error: {error.message}</Text>;
+	}
 
-  return (
-    <View style={styles.grid}>
-      {serviceInfo?.services?.map((data, index) => (
-        <View style={styles.container}>
-          <View key={index} style={styles.card}>
-            <Text style={styles.title}>{data.name}</Text>
-            <View style={styles.imgBox}>
-              <Image style={styles.img} source={{ uri: data.image }} />
-            </View>
-            <View style={styles.lineTime}>
-              <Text style={styles.text}>
-                {data.price}
-                {" VNĐ"}
-              </Text>
-            </View>
-            <View style={styles.lineTime}>
-              <Text style={styles.text}>
-                {data.loyaltyPoints}
-                {" Điểm"}
-              </Text>
-            </View>
-            <TouchableOpacity style={styles.button}>
-              <Text
-                style={styles.buttonText}
-                onPress={() => navigation.navigate("Booking")}
-              >
-                Đặt lịch ngay !
-              </Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      ))}
-    </View>
-  );
+	return (
+		<View style={styles.grid}>
+			{serviceInfo?.services?.map((data, index) => (
+				<View style={styles.container} key={index}>
+					<View style={styles.card}>
+						<Text style={styles.title}>{data.name}</Text>
+						<View style={styles.imgBox}>
+							<Image
+								style={styles.img}
+								source={{ uri: data.image }}
+							/>
+						</View>
+						<View style={styles.lineTime}>
+							<View style={styles.priceContainer}>
+								<Text style={styles.priceText}>
+									{data.price}
+								</Text>
+							</View>
+							<View style={styles.loyaltyContainer}>
+								<Text style={styles.loyalText}>
+									{data.loyaltyPoints} Điểm
+								</Text>
+							</View>
+						</View>
+						<TouchableOpacity style={styles.button}>
+							<Text
+								style={styles.buttonText}
+								onPress={() => navigation.navigate("Booking")}
+							>
+								Đặt lịch ngay !
+							</Text>
+						</TouchableOpacity>
+					</View>
+				</View>
+			))}
+		</View>
+	);
 };
 
 const styles = StyleSheet.create({
-  container: {
-    width: "50%",
-    height: 300,
-  },
-  grid: {
-    flexDirection: "row",
-    flexWrap: "wrap",
-    justifyContent: "space-between",
-  },
-  card: {
-    padding: 10,
-    marginHorizontal: "auto",
-    marginVertical: "auto",
-    width: "90%",
-    height: "90%",
-    backgroundColor: "white",
-    borderRadius: 20,
-    shadowColor: "aqua",
-    shadowOffset: {
-      width: 0,
-      height: 4,
-    },
-    shadowOpacity: 0.5,
-    shadowRadius: 5,
-    elevation: 10,
-  },
-  title: {
-    fontSize: 15,
-    fontWeight: "bold",
-  },
-  titlesub: {
-    fontSize: 10,
-    marginTop: 5,
-  },
-  imgBox: {
-    marginTop: 10,
-    width: "100%",
-    height: "50%",
-    backgroundColor: "aqua",
-    borderRadius: 15,
-  },
-  img: {
-    margin: "auto",
-    width: "92%",
-    height: "90%",
-    borderRadius: 10,
-  },
-  lineTime: {
-    width: "100%",
-    height: 19,
-    marginTop: 10,
-    borderRadius: 20,
-    backgroundColor: "#1D397A",
-    alignItems: "center",
-    justifyContent: "center",
-    marginHorizontal: "auto",
-  },
-  text: {
-    backgroundColor: "white",
-    fontSize: 10,
-    textAlign: "center",
-    height: 16,
-    width: "98%",
-    borderRadius: 20,
-  },
-  button: {
-    width: "100%",
-    height: 30,
-    marginTop: 10,
-    borderRadius: 10,
-    backgroundColor: "#1D397A",
-    alignItems: "center",
-    justifyContent: "center",
-    marginHorizontal: "auto",
-    backgroundColor: "aqua",
-  },
-  buttonText: {
-    color: "#fff",
-    fontSize: 13,
-    fontWeight: "bold",
-  },
+	container: {
+		width: "50%",
+		height: 300,
+	},
+	grid: {
+		flexDirection: "row",
+		flexWrap: "wrap",
+		justifyContent: "space-between",
+	},
+	card: {
+		padding: 8,
+		marginHorizontal: "auto",
+		marginVertical: "auto",
+		width: "94%",
+		height: "94%",
+		backgroundColor: "white",
+		borderRadius: 15,
+		shadowColor: "#5D3A29",
+		shadowOffset: {
+			width: 0,
+			height: 3,
+		},
+		shadowOpacity: 0.5,
+		shadowRadius: 5,
+		elevation: 10,
+	},
+	title: {
+		fontSize: 15,
+		fontWeight: "bold",
+		textAlign: "center",
+	},
+	titlesub: {
+		fontSize: 10,
+		marginTop: 5,
+	},
+	imgBox: {
+		marginTop: 10,
+		width: "100%",
+		height: "60%",
+		backgroundColor: "#5D3A29",
+		borderRadius: 15,
+	},
+	img: {
+		margin: "auto",
+		width: "92%",
+		height: "90%",
+		borderRadius: 10,
+	},
+	lineTime: {
+		width: "100%",
+		height: 19,
+		marginTop: 10,
+		alignItems: "center",
+		justifyContent: "center",
+		marginHorizontal: "auto",
+		flexDirection: "row", // Đặt các phần tử nằm cùng hàng
+		justifyContent: "space-between", // Căn chỉnh khoảng cách giữa giá và điểm
+		alignItems: "center", // Căn giữa theo chiều dọc
+	},
+	priceContainer: {
+		flex: 1, // Chiếm 50% chiều rộng
+		alignItems: "flex-start", // Đặt giá bên trái
+	},
+	loyaltyContainer: {
+		flex: 1, // Chiếm 50% chiều rộng
+		alignItems: "flex-end", // Đặt điểm loyal bên phải
+	},
+	priceText: {
+		fontSize: 15,
+		textAlign: "left",
+		fontWeight: "bold",
+		height: 16,
+		width: "98%",
+		color: "#FF6B6B",
+	},
+	loyalText: {
+		fontSize: 12,
+		textAlign: "right",
+		height: 16,
+		width: "98%",
+	},
+	button: {
+		width: "100%",
+		height: 30,
+		marginTop: 10,
+		borderRadius: 10,
+		alignItems: "center",
+		justifyContent: "center",
+		marginHorizontal: "auto",
+		backgroundColor: "#5D3A29",
+	},
+	buttonText: {
+		color: "#fff",
+		fontSize: 13,
+		fontWeight: "bold",
+	},
 });
 
 export default CardService;
